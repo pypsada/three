89180fb6b2cf8a1d982ff081c9525b09c12f5fa8
Sqrootof <143501283+Sqrootof@users.noreply.github.com>
2024-01-21 00:45:09 +0800

    玩法介绍界面，字体颜色嗯~

---
+++ Assets/UI _C#_Script/IllustrationPannel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IllustrationPannel : MonoBehaviour
+{
+    /*此代码用于调用按键上写好的字符串填入选定的框*/
+
+    public Text Text;
+    public string text;
+
+    public void ShowText()
+    {
+        Text.GetComponent<Text>().text = text;
+        Text.text = Text.text.Replace("\\n", "\n");
+    }
+}
