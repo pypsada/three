47dee6ce8af61e83ea72933436c6596be9f5b745
LiobraveryPlum <145110043+LiobraveryPlum@users.noreply.github.com>
2024-02-06 20:17:54 +0800

    开始编写主游戏网络协议

--- Assets/NetFramework/GameRooms/Proto/RoomMsg.cs
+++ Assets/NetFramework/GameRooms/Proto/RoomMsg.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//ø™ º∆•≈‰
 public class MsgBgmatch : MsgBase
 {
     MsgBgmatch() { protoName = "MsgBgmatch"; }
 }
 
+//Ω· ¯∆•≈‰
 public class MsgMatched : MsgBase
 {
     MsgMatched() { protoName = "MsgMatched"; }
---
+++ Assets/NetFramework/Script/NetMain/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace NetGame
+{
+    public class Player
+    {
+        //id
+        public string id = "";
+        //临时数据
+        public int x;
+        //数据库数据
+        public PlayerData data;
+    }
+}
