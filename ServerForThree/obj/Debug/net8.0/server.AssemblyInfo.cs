bcef5eb741f1c6994b387bb08c438cfaa86583f3
LiobraveryPlum <145110043+LiobraveryPlum@users.noreply.github.com>
2024-03-01 17:58:51 +0800

    服务器自动更改文件

--- ServerForThree/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ ServerForThree/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6d29dcab75c7d9b573ddea42199b06c39cb58d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4c7a859fa9eeea74683c4bcee1df4f6adcdcbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
