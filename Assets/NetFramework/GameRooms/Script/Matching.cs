9a5db49b8eb54dd338f9f376b5471359f66ca85a
LiobraveryPlum <145110043+LiobraveryPlum@users.noreply.github.com>
2024-03-03 13:41:43 +0800

    Revert "尝试修改卡在匹配界面的bug"
    This reverts commit ec471272cdbca54f0536ff4378e767fb246a16eb.

--- Assets/NetFramework/GameRooms/Script/Matching.cs
+++ Assets/NetFramework/GameRooms/Script/Matching.cs
@@ -128,6 +128,7 @@ public class Matching : MonoBehaviour
         Debug.Log("µÇ³ö");
         NetManager.Close();
         NetManager.ping = -1;
+        NetManager.RemoveMsgListener("MsgMatched", Matched);
         SceneManager.LoadScene("FightPrepare");
     }
 
