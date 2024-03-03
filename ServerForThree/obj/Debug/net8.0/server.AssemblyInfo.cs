7a50efa61e20a1f9f9e70f83ad49397032db29cb
LiobraveryPlum <145110043+LiobraveryPlum@users.noreply.github.com>
2024-03-03 23:19:33 +0800

    今天炸库了，重新运行一下VS新加的文件

--- ServerForThree/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ ServerForThree/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4c7a859fa9eeea74683c4bcee1df4f6adcdcbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d770d96ab217116a22e5e0c7b1183296f66db942")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
